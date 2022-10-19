9f15704cc243227644bfae27be8b53bb5d2cae3c
stefanpython <stefanhtml@yahoo.com>
2022-10-19 14:29:31 +0300

    Add function to store, use and retrieve user information from localStorage upon page refresh

--- book.js
+++ book.js
@@ -1,10 +1,23 @@
-const title = document.querySelector('#title');
-const author= document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const read = document.querySelector('#read');
+const titleInput = document.querySelector('#title');
+const authorInput= document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
 
-let counter = 0;
 let myLibrary = [];
+
+const DEFAULT = [
+    {
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      pages: "295",
+      status: "Read",
+    },
+  ];
+
+// Add books from localStorage if pages is refreshed
+if (myLibrary.length === 0) {
+    retrieveData();
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,107 +27,38 @@ function Book(title, author, pages, read) {
 }
 
 
-const addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', (event) => {
-
+const addBookBtn = document.querySelector('#addBtn');
+addBookBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    addBookToLibrary();
+})
 
-    let titleBook = title.value;
-    let authorBook = author.value;
-    let pagesBook = pages.value;
-    let readBook = read.checked;
-    
-    const newBook = new Book(titleBook, authorBook, pagesBook, readBook);
+// Obtain user input information (title, author, pages, read)
+function addBookToLibrary() {
+    // Extract values from user input
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read = readInput.checked;
 
-    myLibrary.push(newBook);
-    displayBook(newBook);
+    // Create a new book object with the new values
+    const book = new Book(title, author, pages, read);
 
+    // Store new book objects in myLibrary array
+    myLibrary.push(book);
     saveData();
     form.reset();
-})
+}
 
 
 // Function to save data to localStorage
-function saveData () {
+function saveData() {
     localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 // Function to retrieve data from localStorage
-function retrieveData () {
-   savedBookList = JSON.parse(localStorage.getItem('myLibrary') || '[]')
+function retrieveData() {
+   myLibrary = JSON.parse(localStorage.getItem('myLibrary'))
 }
 
 
-function displayBook(newBook) {
-
-    const card = document.querySelector('.card');
-
-    let div = document.createElement('div');
-    div.setAttribute('data-num', counter);
-    div.style.width = "13rem";
-    div.style.height = " 13rem";
-    div.style.border = " 4px solid #c31";
-    div.style.display = "flex";
-    div.style.flexDirection = "column";
-    div.style.gap = "10px";
-    div.style.justifyContent = "center";
-    div.style.alignItems = "center";
-    div.style.textAlign = "center";
-    div.style.borderRadius = "5px";
-    div.innerHTML = `
-    <div class="box title">${newBook.title}</div>
-    <div class="box author">${newBook.author}</div>
-    <div class="box pages">${newBook.pages}</div>
-    <div class="box read">${newBook.read}</div>
-    `;
-
-    card.appendChild(div);
-    newBook.dataNum = `${counter}`;
-    counter++;
-    console.log(newBook)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function render() {
-
-//     let boo;
-//     for (let i = 0; i < localStorage.length; i++) {
-//         const key = localStorage.key(i)
-//         let z = localStorage.getItem(key);
-//         let foo = z.split(',');
-//         boo = JSON.parse(foo);
-//       }
-
-//     for (let j=0; j<boo.length; j++){
-//         addBook(boo[j])
-//         console.log(boo[j])
-//     }
-//     return boo;
-// }
-
-
-// function addBook(item) {
-
-//     title.textContent = item.title
-//     author.textContent = item.author
-//     pages.textContent = item.pages
-//     read.textContent = item.read
-// }
